cf01d2f2c93f65ebc3575211e2ecdc272636be3c
F369911 <github.projects911@gmail.com>
2020-10-09 01:18:51 +0330

    first commit

--- carzone/settings.py
+++ carzone/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import secrets
+from . import secrets as sk
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = sk.secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
